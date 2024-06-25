22ef873174cb1257231d0802fa0cc611c8325286
iki-rumondor <ikirumondor07@gmail.com>
2024-06-26 00:34:17 +0800

    fix: ubah url pdf

--- src/utils/Fetching.jsx
+++ src/utils/Fetching.jsx
@@ -5,7 +5,7 @@ const accessToken = sessionStorage.getItem("token");
 // const pdfAPIUrl = "http://localhost:8000/api/pdf/speech";
 
 const baseAPIUrl = "http://103.26.13.166:8080/api";
-const pdfAPIUrl = "http://103.26.13.166:8000/api/pdf/speech";
+const pdfAPIUrl = "http://127.0.0.1:8000/api/pdf/speech";
 
 export const postFile = async (endpoint, method, data) => {
   try {
