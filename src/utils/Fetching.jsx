c4d79ddcdadc22e0f86ccc1b978d16b0d52f5f24
iki-rumondor <ikirumondor07@gmail.com>
2024-06-26 00:23:33 +0800

    fix: mengubah url

--- src/utils/Fetching.jsx
+++ src/utils/Fetching.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const accessToken = sessionStorage.getItem("token");
-const baseAPIUrl = "http://localhost:8080/api";
-const pdfAPIUrl = "http://localhost:8000/api/pdf/speech";
+// const baseAPIUrl = "http://localhost:8080/api";
+// const pdfAPIUrl = "http://localhost:8000/api/pdf/speech";
 
-// const baseAPIUrl = "http://10.11.12.81:8080/api";
-// const pdfAPIUrl = "http://10.11.12.81:8000/api/pdf/speech";
+const baseAPIUrl = "http://103.26.13.166:8080//api";
+const pdfAPIUrl = "http://103.26.13.166:8080//api/pdf/speech";
 
 export const postFile = async (endpoint, method, data) => {
   try {
